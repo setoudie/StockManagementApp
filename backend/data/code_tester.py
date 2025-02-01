f4665d600e0bef1ad5ee0d847880856c5d6b9117
setoudie <setoudie@gmail.com>
2025-02-01 15:54:22 +0000

    frontend first ui template

---
+++ backend/data/code_tester.py
@@ -0,0 +1,2 @@
+dico = dict(Bananes_kg=66, Huile_1L=7485, Lait_1L=14970, Oignons_kg=22085, Poisson_frais=29276, Poulet_frais=36446,
+            Riz_5kg=44009, Sucre_1kg=51157, Tomates_kg=58512, Viandes_kg=65927, Yaourt=73061)
